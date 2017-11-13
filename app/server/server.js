ddb6429e4383f383199fc5c75000242fcc44d9ea
marachi2 <marissaf32@gmail.com>
2017-11-13 14:42:50 -0700

    working with auth

--- app/server/server.js
+++ app/server/server.js
@@ -93,6 +93,7 @@ passport.deserializeUser(function (user, done) {
 });
 
 app.get('/auth/me', (req, res) => {
+    console.log('checking')
     if (!req.user) {
         return res.status(404).send('User not found')
     } else {
